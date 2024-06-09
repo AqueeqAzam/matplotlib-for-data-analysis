c5c7421e2232896477330e552ab3aaa6156ef14f
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2024-06-09 20:57:30 +0530

    Create 0_create_and_syntex.py

---
+++ 0_create_and_syntex.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [2, 4, 6, 8]
+c = ['r', 'y', 'g', 'b']
+
+# linear graph
+# plt.plot(x, y)
+
+# bar graph
+plt.bar(x, y, color=c)
+plt.show()
